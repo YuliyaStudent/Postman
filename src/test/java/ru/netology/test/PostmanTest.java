48793d58c03a4505fc02e73699cb278896392fe1
YuliyaSi <pirozhok_s_kapustpo@mail.ru>
2022-11-05 19:52:40 +0300

    add test

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -16,7 +16,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hell,world!"))
+                .body("data", equalTo("Hello,world!"))
         ;
     }
 }
