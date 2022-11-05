a27fb66c62430cf9154fce4ba6d8d09697384c6e
YuliyaSi <pirozhok_s_kapustpo@mail.ru>
2022-11-05 19:59:40 +0300

    add ruTest

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -9,7 +9,7 @@ public class PostmanTest {
 
     @Test
     public void PostmanTest (){
-                given()
+        given()
                 .baseUri("https://postman-echo.com")
                 .body("Hello,world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 .when()
@@ -17,6 +17,19 @@ public class PostmanTest {
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("Hello,world!"))
+        ;
+    }
+    @Test
+    public void PostmanRuTest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Здравствуйте, коллеги!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Здравствуйте, коллеги!"))
         ;
     }
 }
